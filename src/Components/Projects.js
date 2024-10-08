3fde18292cc62db22e7ce6171c8a04ba404f32b6
Nauman <mohamednauman74@gmail.com>
2024-10-08 21:17:17 +0530

    updated

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -13,21 +13,10 @@ function Projects(){
 
   const project =[
     {
-      name:"Islamic Education Centre",
-      title:"Islamic Education Centre - made by  JavaScript, PHP, and MySQL",
-      link:"https://islamiceducationcentre.com/index.php",
-      id:1,
-      description:" A comprehensive Islamic education platform built with JavaScript (frontend) and PHP/MySQL (backend), offering real-time access to courses and resources. Fully responsive for all devices, it ensures a smooth learning experience. The admin panel handles API integration with CRUD operations, allowing efficient management of users, courses, and content in real-time.",
-      technology:"JavaScript, CSS, HTML, Bootstrap, PHP, and MySql.",
-      img:"islamiceducationcentre.jpg"
-      
-     
-    },
-    {
       name:"Fithub Fitness",
       title:"Gym website using React.js",
       link:"https://naumanmalikfithubfitness.netlify.app/",
-      id:2,
+      id:1,
       description:"A gym website built with React.js showcasing various fitness exercises. Users can explore different exercises, read detailed descriptions about them, and view instructional materials. The website is fully responsive, ensuring a seamless experience across devices.",
       technology:"React.js, JavaScript, CSS, HTML, Bootstrap",
       img:"gym.png"
@@ -37,7 +26,7 @@ function Projects(){
       name:"Weather App",
       title:"Weather app using React.js",
       link:"https://naumanmalikweatherapp.netlify.app/",
-      id:3,
+      id:2,
     description:"A weather application created with React.js that provides current weather conditions, forecasts, and temperature details for different locations.",
     technology:"React.js, JavaScript, CSS, HTML, Bootstrap",
     img:"weather.png"
@@ -48,7 +37,7 @@ function Projects(){
       name:"Todo List",
       title:"TodoList using React.js",
       link:"https://naumanmaliktodolist.netlify.app/",
-      id:4,
+      id:3,
       description:"A simple to-do list application built using React.js. Users can add and delete tasks, organizing their tasks efficiently.",
       technology:"React.js, JavaScript, CSS, HTML, Bootstrap",
       img:"todo.png"
@@ -66,9 +55,6 @@ function Projects(){
 else if(link === "Todo List"){
   return window.location.href =  " https://github.com/mohammednaumanmalik/TodoList-"
 }
-else if(link === "Islamic Education Centre"){
-  return window.location.href =  " https://islamiceducationcentre.com/index.php"
-}
   } 
   function demoBtn(link){
     if(link === "Fithub Fitness"){
@@ -79,9 +65,6 @@ else if(link === "Islamic Education Centre"){
 }
 else if(link === "Todo List"){
   return window.location.href =  " https://naumanmaliktodolist.netlify.app/"
-}
-else if(link === "Islamic Education Centre"){
-  return window.location.href =  " https://islamiceducationcentre.com/index.php"
 }
   }
 
